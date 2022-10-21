fbf8e145ae94e3ffc36d72cba91bf95772d8178d
sarang3232 <72816187+sarang3232@users.noreply.github.com>
2022-10-20 22:41:14 -0500

    Update index.js

--- app1/index.js
+++ app1/index.js
@@ -1,7 +1,7 @@
 const Consul = require('consul');
 const express = require('express');
 
-const SERVICE_NAME='mymicroservice';
+const SERVICE_NAME='web1';
 const SERVICE_ID='m'+process.argv[2];
 const SCHEME='http';
 const HOST='192.168.100.3';
